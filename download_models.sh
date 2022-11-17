66a98ad261de8d1badd0955e2d19e2b92d5e8dca
Travis Driver <travisdriver@gatech.edu>
2022-11-17 12:03:09 -0500

    Model download bash script

---
+++ download_models.sh
@@ -0,0 +1,3 @@
+wget https://www.dropbox.com/s/salv7nbox3k1y9l/hynet_test.onnx
+wget https://www.dropbox.com/s/owi111lh30czg2s/hynet22_test.onnx
+wget https://www.dropbox.com/s/zukj6gem5ww0nsn/superpoint_test.onnx
