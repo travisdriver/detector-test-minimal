c0dc98bb472c353f4a018c0bad82f59d21556e68
Travis Driver <travisdriver@gatech.edu>
2022-11-17 12:55:44 -0500

    Updated model links

--- download_models.sh
+++ download_models.sh
@@ -1,3 +1,5 @@
 wget https://www.dropbox.com/s/salv7nbox3k1y9l/hynet_test.onnx
-wget https://www.dropbox.com/s/owi111lh30czg2s/hynet22_test.onnx
+# wget https://www.dropbox.com/s/owi111lh30czg2s/hynet22_test.onnx
+wget https://www.dropbox.com/s/imidbsrfe4oy4ma/hynet2x2_128_test.onnx
+wget https://www.dropbox.com/s/lxsjij954ykul73/hynet2x2_256_test.onnx
 wget https://www.dropbox.com/s/zukj6gem5ww0nsn/superpoint_test.onnx
