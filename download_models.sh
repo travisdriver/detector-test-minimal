1d88eeb2f2dabad91b71a5d56537e76e6b7186c8
Travis Driver <travisdriver@gatech.edu>
2022-11-17 16:30:00 -0500

    Added SuperPoint descriptor head model

--- download_models.sh
+++ download_models.sh
@@ -3,3 +3,4 @@ wget https://www.dropbox.com/s/salv7nbox3k1y9l/hynet_test.onnx
 wget https://www.dropbox.com/s/imidbsrfe4oy4ma/hynet2x2_128_test.onnx
 wget https://www.dropbox.com/s/lxsjij954ykul73/hynet2x2_256_test.onnx
 wget https://www.dropbox.com/s/zukj6gem5ww0nsn/superpoint_test.onnx
+wegt https://www.dropbox.com/s/x31dssk7znsuppt/superpoint_desc_test.onnx
